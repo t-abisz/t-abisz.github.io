345a1bceb77179d58202bb6ae9c82747316899f6
t-abisz <45064065+t-abisz@users.noreply.github.com>
2018-11-16 11:56:12 +0100

    Create stickyAndToggle.js
    add js

---
+++ media/js/stickyAndToggle.js
@@ -0,0 +1,23 @@
+//cache for buttons
+const cacheClass = {
+  openMenu: document.querySelector('.nav__icon'),
+  headerSticky: document.querySelector('.header')
+}
+//sticky on scroll
+const headerScroll = cacheClass.headerSticky.offsetTop
+window.onscroll = () => sticky();
+function sticky() {
+  if(window.pageYOffset > headerScroll) {
+    headerSticky.classList.add('sticky')
+    } else {
+      headerSticky.classList.remove('sticky')
+      }
+}
+//open/close menu
+const bodyToggle = document.querySelector('body')
+const iconToggle = document.querySelector('.navOpen')
+cacheClass.openMenu.addEventListener('click', () => {
+  cacheClass.openMenu.classList.toggle('open')
+  bodyToggle.classList.toggle('overflow')
+  iconToggle.classList.toggle('height')
+}
